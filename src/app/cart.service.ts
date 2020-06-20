9d2186c8832db7d878da1df7533343089e66c039
jrpool <pool@jpdev.pro>
2020-06-20 10:03:45 -0400

    4 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CartService {
-  items = [];
+  items: object[] = [];
   addToCart(product) {
     this.items.push(product);
   }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,12 +7,11 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
+  items: object[];
   constructor(
     private cartService: CartService
   ) { }
-
   ngOnInit() {
+    this.items = this.cartService.getItems();
   }
-
 }
